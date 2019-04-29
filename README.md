f0f04b13162e12956c2688b70860547c8e3768f3
PEP <borrellpep@gmail.com>
2019-04-29 23:34:12 +0200

    Canvis i millores al codi. Afegit paper.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FISES-Simulacio
 Treball de Simulació de Física Estadística
+
+## Enllaços interessants
+[NumPy for Matlab users](https://docs.scipy.org/doc/numpy/user/numpy-for-matlab-users.html)
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import numpy as np
 
-def acc (xi):
-    return 0.0
+def acc (qi):
+    # Parameter values
+    alpha0 = 1
+    beta0 = 2
+
+    alpha = alpha0/3
+    beta = beta0/4
+    fa = lambda r : -r - alpha*r**2 - beta*r**3
+    a = fa(qi)
+
+    return a
 
 def vverlet_step (x, v, dt, i):
     aux_acc = acc(x[i])
-    x[i+1] = x[i] + v[i]*dt + 0.5*acc(x[i])*dt**2
+    x[i+1] = x[i] + v[i]*dt + 0.5*aux_acc*dt**2
     aux_acc_dt = acc(x[i+1])
     v[i+1] = v[i] + 0.5*(aux_acc + aux_acc_dt)*dt
 
 
 def vverlet (x, v, dt, time):
     for i in range(time-1):
-        vverlet_step(x[i], v[i], dt, i)
-        print(x[i])
+        vverlet_step(x, v, dt, i)
 
 def main():
     time = 100
@@ -24,8 +32,10 @@ def main():
 
     # Initial conditions
     x[0, 0] = 1.0
-    v[0, 0] = -0.0
+    v[0, 0] = 2.0
 
     vverlet(x, v, dt, time)
+
+    print(x[-1])
 
 main()
