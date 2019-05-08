beec065c701794f5a89cfa95c3f990838bbdcf2a
RogerSG11 <rogerserrat11@gmail.com>
2019-05-08 17:43:41 +0200

    Energies i tal

--- simulation.py
+++ simulation.py
@@ -1,24 +1,41 @@
 from tools import *
 
+# SIMULATION PARAMETERS
+
+# Total time = time*dt
 time = 10000
 dt = 1e-3
-size = 20
-x = np.zeros((time, size))
-v = np.zeros((time, size))
+
+#"Time checkpoints"
+tc = [0,1000]
+
+#Size of the system / number of particles
+N = 100
+
+# Potential parameters
+alpha = -1.0
+beta = 2.0
+pot_par = (alpha,beta)
 
 # Initial conditions
-x[0, 1] = 1.0
-v[0, 1] = 0.0
+x = np.zeros(N)
+v = np.zeros(N)
+for i in range(10):
+    x[i] = 10.0*(-1)**i
 
-vverlet(x, v, dt, time)
 
+# SIMULATION: CALCULATION AND PLOT
+E = vverlet(x, v, dt, time, pot_par, tc)
 
-# Plotting
-t = np.arange(0, time)
-fig, ax = plt.subplots(nrows = 5, ncols = 4)
+k = np.arange(N) + 1
+fig, ax = plt.subplots(nrows = np.shape(E)[0])
 i = 0
 for axi in ax:
-    for axij in axi:
-        axij.plot(t, x[:, i])
-        i += 1
+    axi.plot(k, E[i,:])
+    i += 1
 plt.show()
+
+
+# Calculate Ek for 1 <= k <= N
+# Plot Ek w.r.t. k
+# Repeat for different values of time
--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def acc(q):
-    # Parameter values
-    alpha0 = 5.0
-    beta0 = 5.0
+def acc(q, pot_par):
+    # Parameter values for the potential
+    alpha0 = pot_par[0]
+    beta0 = pot_par[1]
 
     alpha = alpha0/3
     beta = beta0/4
@@ -20,16 +20,41 @@ def acc(q):
 
     return a
 
-def vverlet_step(x, v, dt, i):
-    aux_acc = acc(x[i])
-    x[i+1] = x[i] + v[i]*dt + 0.5*aux_acc*dt**2
-    aux_acc_dt = acc(x[i+1])
-    v[i+1] = v[i] + 0.5*(aux_acc + aux_acc_dt)*dt
+def vverlet_step(x, v, dt, pot_par):
+    aux_acc = acc(x, pot_par)
+    x = x + v*dt + 0.5*aux_acc*dt**2
+    aux_acc_dt = acc(x, pot_par)
+    v = v + 0.5*(aux_acc + aux_acc_dt)*dt
 
+# Pre: x and v contain the initial conditions for position and velocity of each particle
+# Post: x and v contain the position and velocity for each particle at time time*dt
+# Retorna una matriu amb l'energia de cada mode per cada temps de tc 
+def vverlet(x, v, dt, time, pot_par, tc):
+    E = np.zeros((len(tc),len(x))) # matrix: time checkpoints x number of modes
+    for t in range(time+1): #+1 per tenir els valors en time
+        vverlet_step(x, v, dt, pot_par)
+        if t in tc:
+            E[tc.index(t)] = calc_Ek(x,v)
+    return E
 
-def vverlet(x, v, dt, time):
-    for i in range(time-1):
-        vverlet_step(x, v, dt, i)
-
-def calc_Pk(v, N, k):
+# Pk: cridar amb v = velocitats
+# Qk: cridar amb v = q = posicions
+def aux_calc_Ek(v):
+    N = len(v)
+    res = np.zeros(N)
+    k = np.arange(N) + 1
+    for i in range(1,N+1):
+        res = res + v*np.sin(np.pi*k*i/(N+1))
+    res = res * np.sqrt(2/(N+1))
+    return res
     
+# q = vector posicions (respecte equilibri)
+# v = vector velocitats
+def calc_Ek(q,v):
+    N = len(q)
+    k = np.arange(N) + 1
+    wk = 2*np.sin(np.pi*k/(2*N+2))
+    Pk = aux_calc_Ek(v)
+    Qk = aux_calc_Ek(q)
+    Ek = Pk**2 + wk**2 * Qk**2
+    return Ek/2
