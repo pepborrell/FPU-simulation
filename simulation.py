515550d10bff83442525d107ed3f288773125133
PEP <borrellpep@gmail.com>
2019-05-17 21:15:43 +0200

    Afegides llicències GNU GPL

--- simulation.py
+++ simulation.py
@@ -42,3 +42,20 @@ for i in range(E.shape[0]):
     ax.plot(k,E[i,:]) #, linefmt=':', markerfmt='C0.')
     ax.set_yscale('log')
     plt.savefig('./plots/energy_modes_{}.png'.format(tc[i]))
+
+'''
+Copyright 2019 Josep Borrell and Roger Serrat
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
